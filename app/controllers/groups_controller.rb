ccbfb82e8608d48a650c947fde3ad677ff0c6bf3
mali <mailfullstack@gmail.com>
2017-01-02 12:51:30 +0800

    implement groups#show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,16 +2,20 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def show
+    @group =Group.find(params[:id])
+  end
+
   def new
     @group = Group.new
   end
+
   def create
     @group = Group.new(group_params)
     @group.save
 
-
       redirect_to groups_path
-
   end
 
   private
@@ -19,5 +23,4 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:title, :description)
   end
-  
 end
